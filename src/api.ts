7c17dc8f664bdae28f453718fdba4b84f0d2e15d
zackary-clark <zackary.a.clark@gmail.com>
2021-10-10 09:12:03 -0400

    Switch to Apollo & GraphQL

--- src/api.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import maxController from "./max/controller";
-
-const api = express();
-
-api.use("/api", maxController);
-
-export default api;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import api from "./api";
+import http from "http";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
+import express from "express";
+import { DocumentNode } from "graphql";
+import { Resolvers } from "../generated/schema";
+import { resolvers } from "./resolvers";
+import typeDefs from "./schema";
 
 const port = process.env.PORT || 8080;
 
-api.listen(port, () => {
-    console.log(`Strongman Tracker API listening on port ${port}!`);
-});
+async function startApolloServer(typeDefs: DocumentNode, resolvers: Resolvers) {
+    const app = express();
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+    await server.start();
+    if (process.env.CLIENT_ORIGIN) app.use(cors({origin: process.env.CLIENT_ORIGIN}));
+    server.applyMiddleware({ app });
+    await httpServer.listen(port);
+    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers);
