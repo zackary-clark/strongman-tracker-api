dc25ba47699f1cba26700bd2b75d3f21e395e500
zackary-clark <zackary.a.clark@gmail.com>
2022-02-12 22:45:42 -0500

    Done switching to Apollo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
+import http from "http";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
 import express from "express";
 import { DocumentNode } from "graphql";
-import http from "http";
 import { Resolvers } from "../generated/schema";
 import { resolvers } from "./resolvers";
 import typeDefs from "./schema";
@@ -19,7 +19,7 @@ async function startApolloServer(typeDefs: DocumentNode, resolvers: Resolvers) {
         plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     });
     await server.start();
-    // if (process.env.CLIENT_ORIGIN) app.use(cors({origin: process.env.CLIENT_ORIGIN}));
+    if (process.env.CLIENT_ORIGIN) app.use(cors({origin: process.env.CLIENT_ORIGIN}));
     server.applyMiddleware({ app });
     await httpServer.listen(port);
     console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
