da48cd115c856a5256515b5a3f0271a2fe20bd66
zackary-clark <zackary.a.clark@gmail.com>
2022-07-17 18:26:57 -0400

    Extreme Logging for Debugging Keycloak Issue

--- src/config/keycloak.ts
+++ src/config/keycloak.ts
@@ -1,12 +1,5 @@
 import Keycloak from "keycloak-connect";
 
-// Keycloak.prototype.redirectToLogin = function() {
-//     // By default, Keycloak attempts to redirect unauthenticated requests to the login page.
-//     // We want everything login(or out) related to go through the client, for now.
-//     // This causes unauthenticated requests to get a 403: Forbidden, instead of redirecting.
-//     return false;
-// };
-
 const realm = process.env.KC_REALM || "Tracker";
 const url = process.env.KC_URL || "http://localhost:8082/";
 const resource = process.env.KC_RESOURCE || "tracker-api";
